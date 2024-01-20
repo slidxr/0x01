244d7bd924788420c0e0e7cf64f611ca56886c6a
Slidxr <47353881+slidxr@users.noreply.github.com>
2024-01-20 06:55:37 +0200

    LSP and autocompletion basic setup

--- lua/0x01/plugins/init.lua
+++ lua/0x01/plugins/init.lua
@@ -3,8 +3,6 @@ return {
 		'akinsho/bufferline.nvim',
 		version = "*",
 		config = function()
-			vim.keymap.set("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
-			vim.keymap.set("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next buffer" })
 			vim.keymap.set("n", "[b", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
 			vim.keymap.set("n", "]b", "<cmd>bnext<cr>", { desc = "Next buffer" })
 			vim.opt.termguicolors = true
